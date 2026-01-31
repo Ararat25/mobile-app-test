d4f8ae0069dacc7b9f52974f54e8a9c54a11636d
Code Agent <code-agent@github.com>
2026-01-31 01:42:36 +0000

    Добавлена документация в README.md и удалены ненужные тестовые файлы.

--- test/new_test_1.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('test function', () {
-    // Проверка конкретной функции приложения
-    expect(appFunction(), equals(expectedResult));
-  });
-}
--- test/new_test_2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('test method interaction', () {
-    // Проверка взаимодействия метода с другими компонентами
-    expect(methodWithInteraction(), equals(expectedResult));
-  });
-}
